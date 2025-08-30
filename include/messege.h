77f97e68406d36a234a90d3c3aec40e96cbfe39b
umange12 <umangshaily05@gmail.com>
2025-08-30 14:53:41 +0530

    Update messege.h

--- include/messege.h
+++ include/messege.h
@@ -1,1 +1,14 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+typedef struct Message {
+    char sender[50];
+    char message[256];
+    struct Message* next;
+} Message;
+
+void sendMessage(char* sender, char* receiver, char* message);
+void displayMessages(char* username);
+
+#endif
 
