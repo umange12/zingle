180f6b4cd8f652ef58a915de1238e30ca64ea959
umange12 <umangshaily05@gmail.com>
2025-08-30 14:52:52 +0530

    Update friend.h

--- include/friend.h
+++ include/friend.h
@@ -1,1 +1,29 @@
+#ifndef FRIEND_H
+#define FRIEND_H
 
+typedef struct Friend {
+    char username[50];
+    struct Friend* next;
+} Friend;
+
+typedef struct Message {
+    char sender[50];
+    char message[256];
+    struct Message* next;
+} Message;
+
+typedef struct UserNode {
+    char username[50];
+    Friend* friends;
+    Message* messages;
+    struct UserNode* next;
+} UserNode;
+
+extern UserNode* userGraph[100];
+extern int userCount;
+
+int addFriend(char* user, char* friend);
+int removeFriend(char* user, char* friend);
+void displayFriends(char* username);
+
+#endif
