770045ce6bdbf046ac1fa512b75c4bf5143f2912
umange12 <umangshaily05@gmail.com>
2025-08-30 14:53:16 +0530

    Update group.h

--- include/group.h
+++ include/group.h
@@ -1,1 +1,25 @@
+#ifndef GROUP_H
+#define GROUP_H
+
+typedef struct GroupMember {
+    char username[50];
+    struct GroupMember* next;
+} GroupMember;
+
+typedef struct Group {
+    char groupName[50];
+    GroupMember* members;
+    struct Message* messages;
+    struct Group* next;
+} Group;
+
+extern Group* groupList;
+extern int groupCount;
+
+int createGroup(char* creator, char* groupName);
+int addMemberToGroup(char* groupName, char* username);
+void sendGroupMessage(char* sender, char* groupName, char* message);
+void displayGroupMessages(char* groupName);
+
+#endif
 
