105f8293f1717a92873bfe86e33818e7b8c99e6d
umange12 <umangshaily05@gmail.com>
2025-08-30 14:52:01 +0530

    Update user.h

--- include/user.h
+++ include/user.h
@@ -1,1 +1,19 @@
+#ifndef USER_H
+#define USER_H
+
+#define HASH_SIZE 101
+
+typedef struct User {
+    char username[50];
+    char password[50];
+    struct User* next;
+} User;
+
+extern User* hashTable[HASH_SIZE];
+
+int usernameExists(char* username);
+int registerUser(char* username, char* password);
+int loginUser(char* username, char* password);
+
+#endif
 
